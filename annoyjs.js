3700327227999186a32cbbed887cd04257a13cbe
Divyansh Raina <57826142+QuantumBoy-729@users.noreply.github.com>
2020-07-25 13:24:06 +0530

    Add files via upload

---
+++ annoyjs.js
@@ -0,0 +1,5 @@
+let str=prompt("are we there yet");
+while(str !== "yes" && str !== "yeah"){
+	str=prompt("Are we there yet");
+}
+alert("YAY we mad it madafaq");		
---
+++ firscr.js
@@ -0,0 +1,7 @@
+//var fn=prompt("What is your first name");
+//var ln=prompt("What is your last name");
+var age=prompt("What is your age");
+//alert("Your Full name is "+fn + " "+ln);
+alert("Your age is "+age*365 + "in days");
+//console.log("Your Full name is "+fn + " "+ln);
+console.log("Your age is "+age*365+"in days");
